571893a241c880feeb272ee6e27371c70895c6fa
Caleb Jones <calebj516@gmail.com>
2021-03-25 21:43:23 -0500

    Passport Facebook 2nd Commit

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  secretKey: "12345-67890-09876-54321",
-  mongoUrl: "mongodb://localhost:27017/nucampsite",
-  facebook: {
-    clientId: "264007785334244",
-    clientSecret: "f50739c29ba56b7b2f15fe3b368ed67e",
-  },
-};
